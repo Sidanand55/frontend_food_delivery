70f4a0901e3489e0b69e4e40fce2e6366ef537e3
Siddharth Anand <siddharth21494@iiitd.ac.in>
2024-09-05 23:57:53 +0530

    n5

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext= createContext(null);
 const StoreContextProvider= (props)=>{
 
     const [cartItems, setCartItems]= useState({});
-    const url= "http://localhost:4000"
+    const url= "https://backend-food-delivery-ofepjn3sl-sidanand55s-projects.vercel.app"
     const [token, setToken]= useState("");
     const [food_list, setFoodList]= useState([])
 
